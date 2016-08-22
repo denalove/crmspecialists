ef31049041cbb0f39620b6dc8b2d3293b6b64c39
Dena Sawyer <denasawyer1111@gmail.com>
2016-08-21 22:55:48 -0400

    fix

--- Consulting/demo-contacts-process.php
+++ Consulting/demo-contacts-process.php
@@ -2,7 +2,8 @@
 session_start();
 
 # Include the Autoloader (see "Libraries" for install instructions)
-require 'vendor/autoload.php';
+require('../vendor/autoload.php');
+
 use Mailgun\Mailgun;
 
 # Instantiate the client.
@@ -23,16 +24,16 @@ if( isset($_POST['submit']) )
 
 
 
-	$to = 'denasawyer1111@gmail.com'; // Replace with your email	
-	$subject = 'Message from website'; // Replace with your $subject
-	$headers = 'From: ' . $_POST['email'] . "\r\n" . 'Reply-To: ' . $_POST['email'];	
+	//$to = 'denasawyer1111@gmail.com'; // Replace with your email	
+	//$subject = 'Message from website'; // Replace with your $subject
+	//$headers = 'From: ' . $_POST['email'] . "\r\n" . 'Reply-To: ' . $_POST['email'];	
 	
-	$message = 'Name: ' . $_POST['name'] . "\n" .
-	           'E-mail: ' . $_POST['email'] . "\n" .
-	           'Subject: ' . $_POST['subject'] . "\n" .
-	           'Message: ' . $_POST['message'];
+	//$message = 'Name: ' . $_POST['name'] . "\n" .
+	//           'E-mail: ' . $_POST['email'] . "\n" .
+	//           'Subject: ' . $_POST['subject'] . "\n" .
+	//           'Message: ' . $_POST['message'];
 	
-	mail($to, $subject, $message, $headers);	
+	//mail($to, $subject, $message, $headers);	
 	header("Location: home.html");
 }
 ?>
