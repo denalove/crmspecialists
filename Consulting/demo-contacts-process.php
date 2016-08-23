65a555d39f8b2bd6761c52600048d3d404354e74
Dena Sawyer <denasawyer1111@gmail.com>
2016-08-22 20:06:33 -0400

    make form work

--- Consulting/demo-contacts-process.php
+++ Consulting/demo-contacts-process.php
@@ -14,13 +14,11 @@ $domain = "sandbox087ac163b21443349994f894259bab00.mailgun.org";
 if( isset($_POST['submit']) )
 {
 	$result = $mgClient->sendMessage("$domain",
-                  array('from'    => 'Mailgun Sandbox <postmaster@sandbox087ac163b21443349994f894259bab00.mailgun.org>',
+                  array('from'    => '$_POST['email']',
                         'to'      => 'Dena Sawyer <denasawyer1111@gmail.com>',
-                        'subject' => 'Hello Dena Sawyer',
-                        'text'    => 'Congratulations Dena Sawyer, you just sent an email with Mailgun!  
-                        You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  
-                        You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain 
-                        so you can send 10,000 emails/month for free.'));
+                        'subject' => '$_POST['subject']',
+                        'text'    => '$_POST['message']',
+                        'name'		=> '$_POST['name']'));
 
 
 
